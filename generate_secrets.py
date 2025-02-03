9479faa2bd23fa9597bd18b05d92482e22868946
RaiAbdullah1800 <nawazabdullah1800@gmail.com>
2025-02-03 12:55:44 +0500

    change env_var_name

--- generate_secrets.py
+++ generate_secrets.py
@@ -5,8 +5,8 @@ secrets = {}
 
 # Read environment variables starting with "STREAMLIT_SECRET_"
 for key, value in os.environ.items():
-    if key.startswith("STREAMLIT_SECRET_"):
-        parts = key[len("STREAMLIT_SECRET_"):].lower().split('__')
+    if key.startswith("STREAMLIT_TOML"):
+        parts = key[len("STREAMLIT_TOML"):].lower().split('__')
         current = secrets
         for part in parts[:-1]:
             current = current.setdefault(part, {})
